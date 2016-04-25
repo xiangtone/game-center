6b278df731aeec9514acc6b6b8916391353e096d
soybean217 <13565644@qq.com>
2016-04-25 17:07:07 +0800

    修改增加二进制接收处理

--- x-demo-js/WebRoot/scripts/t.js
+++ x-demo-js/WebRoot/scripts/t.js
@@ -5,25 +5,48 @@ if (typeof dcodeIO === 'undefined' || !dcodeIO.ProtoBuf) {
 var ProtoBuf = dcodeIO.ProtoBuf;
 var builder = ProtoBuf.loadProtoFile("Apps.proto");
 var ReqGlobalConfig = builder.build("ReqGlobalConfig");
-var msg = new ReqGlobalConfig();
-msg.setGroupsCacheVer('1');
-var contentPb = new Uint8Array(msg.toArrayBuffer());
+var RspGlobalConfig = builder.build("RspGlobalConfig");
+var reqMsg = new ReqGlobalConfig();
+reqMsg.setGroupsCacheVer('1');
+var contentPb = new Uint8Array(reqMsg.toArrayBuffer());
 
-$.ajax({
-	type : "post",
-	url : "http://115.159.125.75/appstore_api",
-	async : true,
-	data : contentPb,
-	dataType : "binary",
-	// dataType : "jsonp",
-	success : function(msg) {
-		console.log(msg);
-	},
-	error : ajaxNetworkError
-});
-function ajaxNetworkError(XMLHttpRequest, textStatus, errorThrown) {
-	// alert("ajaxNetworkError:" + XMLHttpRequest.status + "-" +
-	// XMLHttpRequest.readyState + "-" + textStatus);
-	console.log("ajaxNetworkError:" + XMLHttpRequest.status + "-"
-			+ XMLHttpRequest.readyState + "-" + textStatus + "-");
+url = "http://115.159.125.75/appstore_api";
+var xhr = new XMLHttpRequest();
+xhr.open("post", url, true);
+// xhr.responseType = "blob";
+xhr.responseType = "arraybuffer";
+xhr.onload = function() {
+	if (this.status == 200) {
+		var arrayBuffer = xhr.response;
+		if (arrayBuffer) {
+			console.log("a");
+			var byteArray = new Uint8Array(arrayBuffer);
+			for (var i = 0; i < byteArray.byteLength; i++) {
+				// do something
+				console.log(byteArray[i]);
+			}
+			var rspMsg = RspGlobalConfig.decode(arrayBuffer);
+			console.log(rspMsg.rescode);
+		}
+	}
 }
+xhr.send(contentPb);
+
+// $.ajax({
+// type : "post",
+// url : "http://115.159.125.75/appstore_api",
+// async : true,
+// data : contentPb,
+// dataType : "binary",
+// // dataType : "jsonp",
+// success : function(msg) {
+// console.log(msg);
+// },
+// error : ajaxNetworkError
+// });
+// function ajaxNetworkError(XMLHttpRequest, textStatus, errorThrown) {
+// // alert("ajaxNetworkError:" + XMLHttpRequest.status + "-" +
+// // XMLHttpRequest.readyState + "-" + textStatus);
+// console.log("ajaxNetworkError:" + XMLHttpRequest.status + "-"
+// + XMLHttpRequest.readyState + "-" + textStatus + "-");
+// }
