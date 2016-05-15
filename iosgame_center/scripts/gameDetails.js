77d67292bcc542ce1377ef5e5b1641d2ac2f8d9a
oddshou <oddshou@sina.com>
2016-05-15 19:42:29 +0800

    ios demo

---
+++ iosgame_center/scripts/gameDetails.js
@@ -0,0 +1,91 @@
+﻿var appDetail = null;
+function page_onload() {
+    //读取cooike
+    var jsonString = getCookie("ios_game_app");
+    if (typeof jsonString == "undefined" && jsonString != null && jsonString != ""){
+        return
+    }
+    appDetail = JSON.parse(jsonString);
+    if (appDetail == null) {
+        return
+    }
+    initWithAppInfo(appDetail);
+}
+
+function initWithAppInfo(appdetail) {
+    var html = "";
+    html += "<figure><img src='" +
+        appdetail.IconUrl +
+        "' alt='loading...'><figcaption><h4 style='margin-top:0.1rem '>" +
+        appdetail.ShowName+
+        "</h4><h5>" +
+        appdetail.RecommWord +
+        "</h5></figcaption></figure><a  class='game_Detil_download' href='" +
+        appdetail.PackUrl +
+        "' > 前往App store下载</a>";
+    $(".game_Detil").html(html);
+
+    //添加详情图片
+    var htmlDetails = "";
+    var appPic = appDetail.AppPicUrl.split(",");
+    for(var i = 0; i < appPic.length; i++) {
+        htmlDetails += "<div class='swiper-slide details_img'><img src='" +
+            appPic[i] +
+            "' alt='loading...'/></div>"
+    }
+    $(".swiper-wrapper").html(htmlDetails);
+
+    $(".p_details").html(appDetail.AppDesc);
+    $(".p_details_h").html(appDetail.DevName);
+}
+
+function getCookie(name)//取cookies函数
+{
+    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+    if(arr != null) return (unescape(arr[2])); return null;
+}
+
+// function getappInfo(data){
+//     console.log("------------this is getappInfo log-----------------");
+//     console.log(data);
+//
+//     var html = "";
+//     html += "<figure><img src='"+data.data.iconUrl +"' alt=' " + data.data.showName +"'><figcaption>" +
+//         "<h4>" + data.data.showName + "</h4>" +
+//         "<h5><span>" + data.data.downTimes + "</span></h5>"+
+//         "<h5 id=''><img src='imgs/star-" +(Math.round(data.data.recommLevel / 2))+ ".png' alt='等级'> </h5>"+
+//         "</figcaption></figure><a  class='game_Detil_download' href='" + data.data.packUrl + "' onclick=pushDown(5000000, data.data)>  免费下载（<span id=''>" + (data.data.packSize/ 1048576.0).toFixed(2) +"</span>MB）</a>";
+//     $("div.game_Detil").html(html);
+//
+//     data.data.updateDesc == "" ? $("div#update_detail").hide() : $("div#update_detail p").html(data.data.updateDesc);
+//     $("div#apk_detail p").html(data.data.appDesc);
+//
+//     var appdetails = "";
+//     appdetails += "<h6 class='p_details_h'>开发商：<span id=''>"+data.data.devName +"</span></h6>"+
+//         "<h6 class='p_details_h'>更新日期：<time id=''>"+ getTimeString(data.data.publishTime) +"</time></h6>"+
+//         "<h6 class='p_details_h'>版本号：<span id=''>"+ data.data.verCode +"</span></h6>";
+//
+//     $(".app_details_h").html(appdetails);
+//
+//
+//     var imgs = $(".swiper-slide").children("img");
+//     for(var i = 0; i < 5 && i < imgs.length; i++){
+//         imgs[i].src = data.data.appPicUrl[i];
+//     }
+// }
+
+// function initAppInfo() {
+//     //解析id获取游戏详情
+//     var rx = /[&|#?]appid=[\d]*/
+//     var mc = rx.exec(location.href);
+//     var appId = 0;
+//     if (mc != null) {
+// //			id = parseInt(mc[0]);
+//         appId = mc[0].replace(/[^0-9]/ig, "");
+//         if (isNaN(appId))
+//             appId = 0;
+//     }
+//     getGameDetails(appId, {success: getappInfo});
+// }
+
+// initAppInfo();
---
+++ iosgame_center/scripts/gamecenter.js
@@ -0,0 +1,169 @@
+/**
+ * Created by win7 on 2016/5/13.
+ */
+var pageDone = false;
+var appList = null;
+
+function sendRequest(syncConfigData, successFun) {
+    $.ajax({
+        type: "post",
+        url: "http://127.0.0.1:42010/jsonapi",
+        async: true,
+        // data : contentPb,
+        data: JSON.stringify(syncConfigData),
+        // dataType : "binary",
+        dataType: "json",
+        success: successFun.success,
+        error: ajaxNetworkError
+    });
+
+}
+
+function ajaxNetworkError(XMLHttpRequest, textStatus, errorThrown) {
+    // alert("ajaxNetworkError:" + XMLHttpRequest.status + "-" +
+    // XMLHttpRequest.readyState + "-" + textStatus);
+    console.log("ajaxNetworkError:" + XMLHttpRequest.status + "-"
+        + XMLHttpRequest.readyState + "-" + textStatus + "-");
+}
+
+var syncHead = {
+    "udi": "a=1&b=1",
+    "chnNo": "",
+    "chnPos": "",
+    "clientId": "",
+    "clientPos": "",
+    "clientVer": ""
+}
+
+var syncConfigData = {
+    "header": syncHead,
+    "api": "ReqAppList",
+    "params": {}
+}
+
+var configSuccessFun = {
+    success: function (data) {
+        // console.log(data.data);
+        appList = data.data;
+        // if (window.localStorage) {
+        //     window.localStorage.setItem("globalData", JSON.stringify(data.data.groupInfo));
+        // }
+        console.log("group data success");
+        console.log(appList);
+        if (pageDone){
+            page_onload();
+        }
+    }
+}
+
+sendRequest(syncConfigData, configSuccessFun);
+
+function page_onload() {
+    if (appList == null) {
+        return;
+    }
+
+    var banner = [];
+    var recomm = [];
+    var other = [];
+        for (var i = 0; i < appList.length; i++) {
+            var item = appList[i];
+            if (item.ShowType == 1){
+                banner.push(item);
+            }else if(recomm.length < 8) {
+                recomm.push(item);
+            }else {
+                other.push(item);
+            }
+        }
+    initBanner(banner);
+    initRecomm(recomm);
+    initOther(other);
+}
+
+function initBanner(data) {
+    var ol = "<ol class='carousel-indicators'>";
+    var div = "<div class='carousel-inner'>";
+    for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+        ol += "<li data-target='#myCarousel' data-slide-to='" + i + "'"+ (i == 0 ? "class='active'" : "")+ "></li>";
+        div += "<div class=" +
+            (i==0 ? "'item active'" : "item") +
+            "><a href='" +
+            'iosgame_details.html?appid='+ item.AppID +
+            "' onclick=allClick(" +JSON.stringify(item)+
+            ")><img class='img' src='" +
+            item.AdsPicUrl +
+            "' alt=" +
+            item.ShowName +
+            "></a><div class='carousel-caption'></div></div>"
+    }
+    ol += "</ol>";
+    div += "</div>";
+    $("#myCarousel").html(ol + div);
+    $('.carousel').carousel({
+        interval: 3000
+    });
+}
+
+function initRecomm(data) {
+    var html = "";
+    for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+        html+= "<li><a href='" +
+            'iosgame_details.html?appid=' + item.AppID+
+            "' onclick=allClick("+JSON.stringify(item)+
+            ")><img src='" +
+            item.IconUrl +
+            "' alt=" +
+            'loading...' +
+            "/><p>" +
+            item.ShowName +
+            "</p></a><a  class='download_btn' href='" +
+            item.PackUrl +
+            "'>下载</a></li>"
+    }
+    $(".app_details_regames").html(html);
+}
+
+function initOther(data) {
+    var html = "";
+    for (var i = 0; i < data.length; i++) {
+        var item = data[i];
+        html +="<li class='g_game_li'><a href='" +
+            "iosgame_details.html?appid=" + item.AppID+
+            "' onclick=allClick("+JSON.stringify(item)+
+            ")><figure class='li_figure'><img src='" +
+            item.IconUrl +
+            "' alt='loading...'><figcaption class='li_figure_figcaption'><h4>" +
+            item.ShowName +
+            "</h4><h6>" +
+            item.RecommWord +
+            "</h6></figcaption></figure></a><div class='g_game_r'><a style='display: block'  href= '" +
+            item.PackUrl +
+            "'><button class='btn btn-danger btn-sm btn_new'>下载</button></a></div></li>"
+    }
+    $(".g_game").html(html);
+    
+    
+}
+
+function allClick(item) {
+    // if (appList != null) {
+    //     for(var i = 0; i < appList.length; i++){
+    //         var item = appList[i];
+    //         if (item.AppID == appid) {
+    itemJsonString = JSON.stringify(item);
+    SetCookie("ios_game_app", itemJsonString);
+    //         }
+    //     }
+    // }
+}
+
+function SetCookie(name,value)//两个参数，一个是cookie的名子，一个是值
+{
+    var exp  = new Date();    //new Date("December 31, 9998");
+    exp.setTime(exp.getTime() + 60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+}
+
